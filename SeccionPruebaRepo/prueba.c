82b4db4d1c61b6b064efeffcfaffc59d6547220d
kirake-a <ruben_aalvarado@outlook.com>
2022-03-14 20:08:48 -0600

    Nothing changed

--- SeccionPruebaRepo/prueba.c
+++ SeccionPruebaRepo/prueba.c
@@ -1,7 +1,8 @@
+// This code is just to test the repo, and upstream all the file in the local repo...
 #include <stdio.h>
 
 int main(){
     printf("Hello world!");
-    
+
     return 0;
 }
--- prueba.c
+++
@@ -1,8 +0,0 @@
-// This code is just to test the repo, and upstream all the file in the local repo...
-#include <stdio.h>
-
-int main(){
-    printf("Hello world!");
-
-    return 0;
-}
