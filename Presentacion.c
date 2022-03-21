26095084b655fde99e483343378ff1d5ff722951
kirake-a <ruben_aalvarado@outlook.com>
2022-03-21 01:39:30 -0600

    Funcion presentacion al programa

---
+++ Presentacion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void presentacionInicial();
+
+int main(){
+
+    presentacionInicial();
+    
+    return 0;
+}
+
+void presentacionInicial(){
+    printf("Bienvenido a este minijuego. \n\nCaperucita Roja. The game...");
+    printf("\n\nRecuerda que cada una de las decisiones que tomes influirá en como terminarás el juego... intenta no morir c:");
+}
